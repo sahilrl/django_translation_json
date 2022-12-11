d2363dad10fdfd97eefa3bafaf50fa95e98e0ac6
sahilrl <imsahilrl@gmail.com>
2022-12-11 15:03:32 +0000

    abstract model added

--- books/booklist/models.py
+++ books/booklist/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 
 
-class Books(models.Model):
+class AbstractModel(models.Model):
+    locale = models.JSONField()
+    
+    class Meta:
+        abstract = True
+
+
+class Books(AbstractModel):
     title = models.CharField(max_length=500, blank=False, null=False)
     author = models.CharField(max_length=500, null=True, blank=True)
     genre = models.CharField(max_length=500, null=True, blank=True)
-    locale = models.JSONField()
-
+   
     def __str__(self):
         return self.title
 
    
-class Library(models.Model):
+class Library(AbstractModel):
     name = models.CharField(max_length=500, blank=False, null=False)
     location = models.CharField(max_length=500, null=True, blank=True)
-    locale = models.JSONField()
     
     def __str__(self):
         return self.name
